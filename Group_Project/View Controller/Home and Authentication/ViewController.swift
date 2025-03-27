34abd42d61bdeb16f754fb3812642dd17b0be9d6
Fuwad <oladegafuwad7@gmail.com>
2025-03-27 09:37:06 -0400

    all controllers addded

---
+++ Group_Project/View Controller/Home and Authentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group_Project
+//
+//  Created by Fuwad Oladega on 2025-03-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
